aff19fbe3c0f3b1a87081615f405ad76d3066eb9
Jerry Blåfield <K9105@student.jamk.fi>
2018-02-05 17:04:50 +0200

    vko6 esim

---
+++ Collections2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var persons = new List<Person>();
+
+            persons.Add(new Person { Firstname = "Jani", Lastname = "Immonen", SocialSecurityNumber = "123214124" });
+            persons.Add(new Person { Firstname = "Pertti", Lastname = "Kettu", SocialSecurityNumber = "7457547" });
+            persons.Add(new Person { Firstname = "Damien", Lastname = "Bolognese", SocialSecurityNumber = "47554334" });
+
+            //yhen tyypin data
+            //Console.WriteLine(persons.ElementAt(0).ToString());
+            //
+            //Console.WriteLine(persons[0].ToString());
+
+            foreach (Person person in persons)
+            {
+                Console.WriteLine(person.ToString());
+                if (person == persons.Last())
+                {
+                    Console.WriteLine("Last!");
+                }
+            }
+
+            //persons.Sort((x, y) => x.Lastname.CompareTo(y.Lastname)); // fuckken magic
+            persons.Sort();
+            foreach (Person person in persons)
+            {
+                Console.WriteLine(person.ToString());
+            }
+
+            /*var names = new List<string>
+            {
+                "Anna",
+                "Pekka",
+                "Kimmo",
+                "Daavetti"
+            };
+
+            names.ForEach(name => Console.WriteLine($"Hello {name}")); //dollarilla muuttujia voi laittaa lainausmerkkien sisään
+            */
+            /*
+             names.ForEach(ShowInConsole);
+             */
+        }
+
+        /*private static void ShowInConsole(string txt)
+        {
+            Console.WriteLine(txt);
+        }
+        */
+    }
+}
---
+++ CollectionsStack/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollectionsStackQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //stack
+            /*// create stack for strings
+            Stack<string> stackStrings = new Stack<string>();
+
+            stackStrings.Push("first");
+            stackStrings.Push("second");
+            stackStrings.Push("third");
+
+            //get first
+            Console.WriteLine(stackStrings.Peek());
+
+            // print all
+            foreach (string s in stackStrings)
+            {
+                Console.WriteLine(s);
+            }
+
+            // get and delete first
+            string temp = stackStrings.Pop();
+            Console.WriteLine("Just popped: {0}", temp);
+            Console.WriteLine("Stack Count is {0}", stackStrings.Count); // count is 2*/
+
+            //queue
+            /*// create queue instance
+            Queue<string> queue = new Queue<string>();
+            // add values
+            queue.Enqueue("first");
+            queue.Enqueue("second");
+            queue.Enqueue("third");
+            // get first and leave it to collection
+            string first = queue.Peek();
+            Console.WriteLine("first in queue is {0}", first); // first
+            Console.WriteLine("Count is {0}", queue.Count); // 3
+                                                            // get first and remove it from collecion
+            first = queue.Dequeue();
+            Console.WriteLine("first in queue is {0}", first);  // first
+            Console.WriteLine("Count is {0}", queue.Count); // 2
+
+            */
+
+            //Dictionary
+            // create persons collection
+            Dictionary<string, Person> persons = new Dictionary<string, Person>();
+            // create persons
+            Person person1 = new Person { Firstname = "Kirsi", Lastname = "Mainio", SocialSecurityNumber = "010190-111A" };
+            Person person2 = new Person { Firstname = "Matti", Lastname = "Husso", SocialSecurityNumber = "020292-222A" };
+            Person person3 = new Person { Firstname = "Teppo", Lastname = "Vuolle", SocialSecurityNumber = "030393-333A" };
+            // add persons to collection
+            persons.Add(person1.SocialSecurityNumber, person1);
+            persons.Add(person2.SocialSecurityNumber, person2);
+            persons.Add(person3.SocialSecurityNumber, person3);
+            // find person with key
+            string ssn = "020292-222A";
+            Person found;
+            if (persons.TryGetValue(ssn, out found))
+            {
+                Console.WriteLine("Person with social security number {0} is {1}.", ssn, found.ToString());
+                // output
+                //  Person with social security number 020292-222A is Matti.
+
+            }
+            else
+            {
+                Console.WriteLine("Person with social security number {0} is {1}.", ssn);
+            }
+
+            // go through all keys
+            foreach (string key2 in persons.Keys)
+            {
+                Console.WriteLine(key2);
+            }
+            //go through all values
+            foreach (Person person in persons.Values)
+            {
+                Console.WriteLine(person);
+            }
+            // go through all keys and values
+            foreach (KeyValuePair<string, Person> kvp in persons)
+            {
+                Console.WriteLine("{0}:{1}", kvp.Key, kvp.Value);
+            }
+            // remove object
+            if (persons.ContainsKey(ssn))
+            {
+                persons.Remove(ssn);
+            }
+
+            // size
+            Console.WriteLine("Collection size is {0}", persons.Count); // 2
+        }
+    }
+}
