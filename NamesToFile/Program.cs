15eb585a8858772c783c0142d43ed201e1274c97
Jerry Blåfield <K9105@student.jamk.fi>
2018-02-08 18:40:16 +0200

    poikkeuksienkäsittely esimerkki

---
+++ NamesToFile/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO; //tiedostojen käsittelyä varten
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamesToFile
+{
+    class Program
+    {
+        static void Test()
+        {
+
+            try
+            {
+                string line;
+                string myfile = @"D:\K9105\testi.txt";
+                //luodaan tiedosto
+                StreamWriter sw = new StreamWriter(myfile);
+                //kysytään käyttäjältä nimet ja kirjoitetaan nimet tiedostoon
+                do
+                {
+                    Console.WriteLine("Anna henkilön nimi (tyhjä lopettaa)");
+                    line = Console.ReadLine();
+                    if (line.Length > 0)
+                        sw.WriteLine(line);
+                } while (line.Length > 0);
+                sw.Close();
+                //avataan tiedosto lukua varten
+                if (File.Exists(myfile))
+                {
+                    //näytetään nimet
+                    string txt = File.ReadAllText(myfile);
+                    Console.WriteLine("Tiedostossa {0} on nimet: \n{1}", myfile, txt);
+                }
+                else
+                {
+                    Console.WriteLine("Tiedostoa {0} ei löydy", myfile);
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Test();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
